01eb079e00d8851c4a343e424e1fbc68a552f7cf
genki kitamura <www.08056561815@gmail.com>
2019-01-31 20:01:39 +0900

    Delete console.log

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -21,7 +21,6 @@ $(document).on('turbolinks:load', function() {
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
       $.ajax({
       type: 'GET',
       url: '/users',
