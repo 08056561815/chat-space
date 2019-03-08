236cd9209b0c0c7144a841bf16126a069ca50e21
genki kitamura <www.08056561815@gmail.com>
2019-03-08 12:28:57 +0900

    Add unicorn

---
+++ spec/factories/messages.rb
@@ -0,0 +1,11 @@
+include ActionDispatch::TestProcess
+
+FactoryBot.define do
+  factory :message do
+    content {Faker::Lorem.sentence}
+    # image {File.open("#{Rails.root}/public/images/no_image.jpg")}
+    image { Rack::Test::UploadedFile.new(Rails.root.join('public/images/no_image.jpg'), 'image/jpg') }
+    user
+    group
+  end
+end
